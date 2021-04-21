6a5e53b3059095b8f1c9f6c727d2f68e1052d39d
tylercmac <tylercmac@gmail.com>
2021-04-20 17:17:09 -0700

    basic server construction

---
+++ Develop/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'))
+
+app.get(`/`, (req, res) => res.sendFile(path.join(__dirname, `/public/index.html`)));
+
+app.get(`/notes`, (req, res) => res.sendFile(path.join(__dirname, `/public/notes.html`)));
+
+
+
+
+
+app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
