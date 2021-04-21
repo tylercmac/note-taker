2e4c437b23dd48b7806aae3ed8042ac8e250bcef
tylercmac <tylercmac@gmail.com>
2021-04-21 09:42:55 -0700

    quirks wit hfile placements

--- server.js
+++ server.js
@@ -7,13 +7,13 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static('public'))
+app.use(express.static('develop/public'))
 
 let notes = [];
 
-app.get(`/`, (req, res) => res.sendFile(path.join(__dirname, `/public/index.html`)));
+app.get(`/`, (req, res) => res.sendFile(path.join(__dirname, `/develop/public/index.html`)));
 
-app.get(`/notes`, (req, res) => res.sendFile(path.join(__dirname, `/public/notes.html`)));
+app.get(`/notes`, (req, res) => res.sendFile(path.join(__dirname, `/develop/public/notes.html`)));
 
 app.get(`/api/notes`, (req, res) => res.json(notes));
 
